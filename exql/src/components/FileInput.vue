863ad35950bfbfe41de8dece5481549c60ac1473
danny <40965142+dbdoan@users.noreply.github.com>
2026-05-17 01:28:25 -0500

    front end setup
    - initialized scaffolding/structure with vite
    - added file picker
    - submission btn
    - footer
    
    to-do:
    - animate footer social btns
    - link submit btn to do something

---
+++ exql/src/components/FileInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <section id="center">
+    <!-- <div class="hero">
+    </div> -->
+    
+    <div>
+      <h1>EX-QL</h1>
+      <p>Upload a <code>Excel (.xlsx)</code> file to begin</p>
+    </div>
+
+    <div class="mb-0">
+      <input class="form-control" type="file" id="formFile" accept=".xlsx">
+    </div>
+
+    <div class="mb-0">
+      <button type="button" class="btn btn-dark">
+        Submit
+
+      </button>
+    </div>
+  </section>
+
+  <div class="ticks"></div>
+  <div class="ticks"></div>
+
+  <section id="spacer"></section>
+
+  <footer class="mb-2">
+    <div>
+      developed by danny
+    </div>
+    <div>
+      <a href="https://www.linkedin.com/in/dbdoan/" target="_blank">
+        <img :src="linkedinLogo" class="linkedin-logo" width="25" height="25">
+      </a>
+      <a href="https://github.com/dbdoan" target="_blank">
+        <img :src="githubLogo" class="github-logo ms-1" width="25" height="25">
+      </a>
+    </div>
+  </footer>
+
+</template>
+
+<script setup lang="ts">
+  // import { ref } from 'vue'
+  import linkedinLogo from '../assets/linkedin_logo.svg';
+  import githubLogo from '../assets/github_logo.svg';
+</script>
